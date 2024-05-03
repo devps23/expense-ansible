6c07b8dc3b869fa18132754fd1d904d9ee79bc56
Pavani <vemasani.pavani@gmail.com>
2024-05-03 10:19:12 +0530

    pass env command dynamically

--- ansible.sh
+++ ansible.sh
@@ -1,2 +1,3 @@
 component=$1
-ansible-playbook -i 172.31.28.66, expense.yml -e component_name=$component -e env=dev -e ansible_user=ec2-user -e ansible_password=DevOps321
+env
+ansible-playbook -i 172.31.28.66, expense.yml -e component_name=$component -e env=$env -e ansible_user=ec2-user -e ansible_password=DevOps321

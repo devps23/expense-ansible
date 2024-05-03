c4a4bca98819afe8e5095178f4dc3e56177d5572
Pavani <vemasani.pavani@gmail.com>
2024-05-03 10:18:12 +0530

    pass component dynamically

--- ansible.sh
+++ ansible.sh
@@ -1,2 +1,2 @@
 component=$1
-ansible-playbook -i 172.31.28.66, expense.yml -e component_name={{component}} -e env=dev -e ansible_user=ec2-user -e ansible_password=DevOps321
+ansible-playbook -i 172.31.28.66, expense.yml -e component_name=$component -e env=dev -e ansible_user=ec2-user -e ansible_password=DevOps321

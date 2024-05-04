ea56ad02148c9b2374d6a4288295b0bd0f04724a
Pavani <vemasani.pavani@gmail.com>
2024-05-05 00:19:12 +0530

    create datasource for existing resource

--- terraform-module-struct/module/app/main.tf
+++ terraform-module-struct/module/app/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "frontend" {
   ami = var.aws_ami
   instance_type = var.aws_instance_type
-  vpc_security_group_ids = [data.aws_security_group.selected]
+  vpc_security_group_ids = [data.aws_security_group.selected.id]
   tags = {
     Name = "frontend-1"
   }

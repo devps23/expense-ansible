0dc1fcc0eb98bb8b0ab64ea5e921bfb855492487
Pavani <vemasani.pavani@gmail.com>
2024-05-04 23:41:49 +0530

    create backend and mysql

--- resource_create/main.tf
+++ resource_create/main.tf
@@ -5,3 +5,17 @@ resource "aws_instance" "resource" {
     Name = "frontend"
   }
 }
+resource "aws_instance" "resource" {
+  ami = "ami-090252cbe067a9e58"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "backend"
+  }
+}
+resource "aws_instance" "resource" {
+  ami = "ami-090252cbe067a9e58"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "mysql"
+  }
+}

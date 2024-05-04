3e53f44a70aa4d38ca0694241726caad3660e304
Pavani <vemasani.pavani@gmail.com>
2024-05-04 23:49:33 +0530

    load value of variables in .tfvars

--- resource_create/main.tf
+++ resource_create/main.tf
@@ -1,20 +1,20 @@
 resource "aws_instance" "frontend" {
-  ami = "ami-090252cbe067a9e58"
-  instance_type = "t2.micro"
+  ami = var.ami
+  instance_type = var.instance_type
   tags = {
     Name = "frontend-1"
   }
 }
 resource "aws_instance" "backend" {
-  ami = "ami-090252cbe067a9e58"
-  instance_type = "t2.micro"
+  ami = var.ami
+  instance_type = var.instance_type
   tags = {
     Name = "backend-1"
   }
 }
 resource "aws_instance" "mysql" {
-  ami = "ami-090252cbe067a9e58"
-  instance_type = "t2.micro"
+  ami = var.ami
+  instance_type = var.instance_type
   tags = {
     Name = "mysql-1"
   }

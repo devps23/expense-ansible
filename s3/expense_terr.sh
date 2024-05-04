e30f23622fa5f081b027ed3affa51584a8a86c15
Pavani <vemasani.pavani@gmail.com>
2024-05-05 01:10:24 +0530

    missed /

--- s3/expense_terr.sh
+++ s3/expense_terr.sh
@@ -5,12 +5,10 @@ if [ -z "$env" ]; then
   echo "env is missing"
   exit 1
 fi
-
 if [ -z "$action" ]; then
  echo "action is missing"
  exit 1
 fi
-
 rm -rf .terraform/terraform.tfstate
 terraform init -backend-config=s3/multistate_bkt/env-$env/state.tfvars
 terraform $action -var-file=s3/multistate_bkt/env-$env/main.tfvars

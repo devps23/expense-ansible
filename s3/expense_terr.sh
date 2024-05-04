b37dcce05095f1218fc89c9b0f707662ae86ed08
Pavani <vemasani.pavani@gmail.com>
2024-05-05 01:04:30 +0530

    move .sh file in s3

--- s3/expense_terr.sh
+++ s3/expense_terr.sh
@@ -3,10 +3,12 @@ action=$2
 # here z is an empty in shell
 if [ -z "$env" ]; then
   echo "env is missing"
+  exit 1
 fi
 
 if [ -z "$action" ]; then
  echo "action is missing"
+ exit 1
 fi
 
 rm -rf .terraform/terraform.tfstate

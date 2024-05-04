9103ddb6cdac6a647468c4e37da989e4dcb061e8
Pavani <vemasani.pavani@gmail.com>
2024-05-04 23:13:47 +0530

    run terraform through shell

---
+++ expense_terr.sh
@@ -0,0 +1,14 @@
+env=$1
+action=$2
+# here z is an empty in shell
+if [ -z "$env" ]; then
+  echo "env is missing"
+fi
+
+if [ -z "$action" ]; then
+ echo "action is missing"
+fi
+
+rm -rf .terraform/terraform.tfstate
+terraform init -backend-config=s3/multistate_bkt/env-$env=state.tfvars
+terraform $action -var-file=s3/multistate_bkt/env-$env=main.tfvars

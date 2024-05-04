93e2d112350f4f05b641312601251b35bbe73f6d
Pavani <vemasani.pavani@gmail.com>
2024-05-04 23:42:53 +0530

    resource local name must be unique

--- resource_create/main.tf
+++ resource_create/main.tf
@@ -1,18 +1,18 @@
-resource "aws_instance" "resource" {
+resource "aws_instance" "frontend" {
   ami = "ami-090252cbe067a9e58"
   instance_type = "t2.micro"
   tags = {
     Name = "frontend"
   }
 }
-resource "aws_instance" "resource" {
+resource "aws_instance" "backend" {
   ami = "ami-090252cbe067a9e58"
   instance_type = "t2.micro"
   tags = {
     Name = "backend"
   }
 }
-resource "aws_instance" "resource" {
+resource "aws_instance" "mysql" {
   ami = "ami-090252cbe067a9e58"
   instance_type = "t2.micro"
   tags = {

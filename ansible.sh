d06d11ca0a8c278412bd168c8038aa3cf3def2ab
Pavani <vemasani.pavani@gmail.com>
2024-05-08 11:40:06 +0530

    call dynamically

--- ansible.sh
+++ ansible.sh
@@ -1,5 +1,5 @@
 component=$1
 env=$2
-ansible-playbook -i 172.31.31.14, expense.yml -e component_name=$component -e env=$env -e ansible_user=ec2-user -e ansible_password=DevOps321
+ansible-playbook -i $component-$env.pdevops72.online, expense.yml -e component_name=$component -e env=$env -e ansible_user=ec2-user -e ansible_password=DevOps321
 
 

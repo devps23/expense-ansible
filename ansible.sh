ec7ed5aaf6ef7235dc84e79e71fc8f205f338676
Pavani <vemasani.pavani@gmail.com>
2024-05-28 17:28:20 +0530

    to get secrets from vault...

--- ansible.sh
+++ ansible.sh
@@ -1,6 +1,6 @@
 component=$1
 env=$2
 ansible-playbook -i $component-$env.pdevops72.online, get_secrets_vault.yml -e component_name=$component -e env=$env
-ansible-playbook -i 10.10.0.148, expense.yml -e component_name=$component -e env=$env -e '@~/secrets.json'
+ansible-playbook -i $component-$env.pdevops72.online, expense.yml -e component_name=$component -e env=$env -e '@secrets.json'
 
 

1cf2c63ee0ac10ef06e5d1200dfb3486cb5ccfa7
Pavani <vemasani.pavani@gmail.com>
2024-05-28 16:50:45 +0530

    get secrets data variables from files to ansible

--- ansible.sh
+++ ansible.sh
@@ -1,6 +1,6 @@
 component=$1
 env=$2
-#ansible-playbook -i $component-$env.pdevops72.online, get_secrets_vault.yml -e component_name=$component -e env=$env
-ansible-playbook -i $component-$env.pdevops72.online, expense.yml -e component_name=$component -e env=$env -e ansible_user=ec2-user -e ansible_password=DevOps321
+ansible-playbook -i $component-$env.pdevops72.online, get_secrets_vault.yml -e component_name=$component -e env=$env
+ansible-playbook -i $component-$env.pdevops72.online, expense.yml -e component_name=$component -e env=$env -e "@secrets.json"
 
 

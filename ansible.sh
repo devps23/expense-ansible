9d24ccc6dc5481f14796c97cd95e5259555ea060
Pavani <vemasani.pavani@gmail.com>
2024-05-28 20:06:01 +0530

    retrieve root password from vault

--- ansible.sh
+++ ansible.sh
@@ -1,6 +1,6 @@
 component=$1
 env=$2
 ansible-playbook -i $component-$env.pdevops72.online, get_secrets_vault.yml -e vault_token=$vault_token -e component_name=$component -e env=$env
-ansible-playbook -i $component-$env.pdevops72.online, expense.yml -e component_name=$component -e env=$env -e '@secrets.json'
+ansible-playbook -i $component-$env.pdevops72.online, expense.yml -e component_name=$component -e env=$env -e '@secrets.json' -e '@app.json'
 
 

ca086c750748f3b38d6479e66dbd557782f50e1d
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-07-08 22:31:37 +0530

    add user as artifact

--- ansible.sh
+++ ansible.sh
@@ -1,6 +1,7 @@
 component=$1
 env=$2
-ansible-playbook -i $component-$env.pdevops72.online, get_secrets_vault.yml -e vault_token=$vault_token -e component_name=$component -e env=$env
+#ansible-playbook -i $component-$env.pdevops72.online, get_secrets_vault.yml -e vault_token=$vault_token -e component_name=$component -e env=$env
+ansible-playbook -i s get_secrets_vault.yml -e vault_token=$vault_token -e component_name=$component -e env=$env
 ansible-playbook -i $component-$env.pdevops72.online, expense.yml -e component_name=$component -e env=$env -e '@secrets.json' -e '@app.json'
 
 

bf8efa28ea1820b7b8c62bc14ded261879825008
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-07-12 23:07:14 +0530

    hosts added in gettscrets

--- ansible.sh
+++ ansible.sh
@@ -1,7 +1,7 @@
 component=$1
 env=$2
-#ansible-playbook -i $component-$env.pdevops72.online, get_secrets_vault.yml -e vault_token=$vault_token -e component_name=$component -e env=$env
-ansible-playbook -i s get_secrets_vault.yml -e vault_token=$vault_token -e component_name=$component -e env=$env
+ansible-playbook -i 172.31.38.92, get_secrets_vault.yml -e vault_token=$vault_token -e component_name=$component -e env=$env
+#ansible-playbook -i s get_secrets_vault.yml -e vault_token=$vault_token -e component_name=$component -e env=$env
 ansible-playbook -i 172.31.38.92, expense.yml -e component_name=$component -e env=$env -e '@secrets.json' -e '@app.json'
 
 

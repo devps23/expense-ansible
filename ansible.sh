77e76511de68a608736b6a3730230ef0b4454a18
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-07-12 22:25:50 +0530

    vault secrets encrypted

--- ansible.sh
+++ ansible.sh
@@ -2,6 +2,6 @@ component=$1
 env=$2
 #ansible-playbook -i $component-$env.pdevops72.online, get_secrets_vault.yml -e vault_token=$vault_token -e component_name=$component -e env=$env
 ansible-playbook -i s get_secrets_vault.yml -e vault_token=$vault_token -e component_name=$component -e env=$env
-ansible-playbook -i $component-$env.pdevops72.online, expense.yml -e component_name=$component -e env=$env -e '@secrets.json' -e '@app.json'
+ansible-playbook -i 172.31.38.92, expense.yml -e component_name=$component -e env=$env -e '@secrets.json' -e '@app.json'
 
 

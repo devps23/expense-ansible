2e6c40c94dd33c2e28504fa8d4a6d99f4debfcb9
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-07-14 07:29:57 +0530

    1. changed vault path 2.added componentn hosts

--- ansible.sh
+++ ansible.sh
@@ -1,7 +1,10 @@
 component=$1
 env=$2
-#ansible-playbook -i $component-$env.pdevops72.online, get_secrets_vault.yml -e vault_token=$vault_token -e component_name=$component -e env=$env
-ansible-playbook -i s get_secrets_vault.yml -e vault_token=$vault_token -e component_name=$component -e env=$env
-ansible-playbook -i $component-$env.pdevops72.online, expense.yml -e component_name=$component -e env=$env -e '@secrets.json' -e '@app.json'
+ansible-playbook  get_secrets_vault.yml -e vault_token=$vault_token -e component_name=$component -e env=$env
+ansible-playbook -i 172.31.38.92, expense.yml -e component_name=$component -e env=$env -e '@secrets.json' -e '@app.json'
+
+#aws ec2 describe-instances --filters "Name=tag:Name,Values=$component-$env"  --query "Reservations[*].Instances[*].PrivateIpAddress" --output text >inv
+#ansible-playbook -i inv, expense.yml -e component_name=$component -e env=$env -e '@secrets.json' -e '@app.json'
+#ansible-playbook -i $component-$env.pdevops72.online, expense.yml -e component_name=$component -e env=$env -e '@secrets.json' -e '@app.json's
 
 

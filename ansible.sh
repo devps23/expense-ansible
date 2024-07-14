87a0021cb1fc30f47afcdd0f3acac249f1d41f55
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-07-14 20:33:30 +0530

    pass dynamically

--- ansible.sh
+++ ansible.sh
@@ -1,7 +1,7 @@
 component=$1
 env=$2
 # shellcheck disable=SC2154
-ansible-playbook  get_secrets_vault.yml -e vault_token=$vault_token -e component_name=$component
+ansible-playbook  get_secrets_vault.yml -e vault_token=$vault_token -e component_name=$component > tmp
 #ansible-playbook -i 172.31.35.48, expense.yml -e component_name=$component -e env=$env -e '@secrets.json' -e '@app.json'
 ansible-playbook -i 172.31.38.92, expense.yml -e component_name=$component -e env=$env -e ansible_user=ec2-user -e ansible_password=DevOps321
 

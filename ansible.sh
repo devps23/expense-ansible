4e72d075f11c9d34f8c98b9d9971a424f1815204
PavaniVemasani18 <vemasani.pavani@gmail.com>
2024-07-14 20:24:32 +0530

    added localhost.....

--- ansible.sh
+++ ansible.sh
@@ -2,7 +2,7 @@ component=$1
 env=$2
 # shellcheck disable=SC2154
 ansible-playbook  get_secrets_vault.yml -e vault_token=$vault_token -e component_name=$component
-ansible-playbook -i 172.31.38.92, expense.yml -e component_name=$component -e env=$env -e '@secrets.json' -e '@app.json'
+ansible-playbook -i 172.31.35.48, expense.yml -e component_name=$component -e env=$env -e '@secrets.json' -e '@app.json'
 
 #aws ec2 describe-instances --filters "Name=tag:Name,Values=$component-$env"  --query "Reservations[*].Instances[*].PrivateIpAddress" --output text >inv
 #ansible-playbook -i inv, expense.yml -e component_name=$component -e env=$env -e '@secrets.json' -e '@app.json'
